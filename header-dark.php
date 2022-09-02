d4a1416b493f41265adee22caa71d2243dc0ded7
justinchodges <justin.hodges@digitellie.com>
2022-09-01 21:45:01 -0500

    added dark header option

---
+++ header-dark.php
@@ -0,0 +1,43 @@
+<!doctype HTML>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        <div class="app">
+            <header class="header header-dark">
+                <div class="container p-lg-x-5">
+                    <div class="row align-items-center g-lg-3">
+                        <div class="column">
+                            <a href="/">
+                                <img class="header__logo" src="/wp-content/themes/iran-alive/assets/images/iran-alive.png" alt="Iran Alive logo" />
+                            </a>
+                        </div>
+                        <div class="column-none justify-self-end">
+                            <nav class="main-nav">
+                                <div class="d-lg-flex g-lg-3 align-items-center">
+                                    <div class="main-nav__item column-none">
+                                        <a class="main-nav__link" href="/stories/">Stories</a>
+                                    </div>
+                                    <div class="main-nav__item column-none">
+                                        <a class="main-nav__link" href="/prayer/">Prayer</a>
+                                    </div>
+                                    <div class="main-nav__item column-none">
+                                        <a class="main-nav__link" href="/about/">About</a>
+                                    </div>
+                                    <div class="main-nav__item column-none">
+                                        <a class="button button-primary" href="#">Donate</a>
+                                    </div>
+                                </div>
+                            </nav>
+                            <button class="hamburger" aria-label="Open navigation">
+                                <div class="hamburger__bar"></div>
+                                <div class="hamburger__bar"></div>
+                                <div class="hamburger__bar"></div>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <div class="page">
---
+++ page-header-dark.php
@@ -0,0 +1,5 @@
+<?php
+/* Template Name: Header Dark */
+get_header( 'dark' );
+the_content();
+get_footer();
